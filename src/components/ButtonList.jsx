83136e914dd3cd7bd366bebb7c9c101f218eb03b
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-06-28 15:39:20 +0530

    Add Buttonlist scoll buttons

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="flex overflow-scroll w-[90vw]">
+      <Button name="All" />
+      <Button name="Game" />
+      <Button name="Live" />
+      <Button name="Songs" />
+      <Button name="Movies" />
+      <Button name="Motivation" />
+      <Button name="Gadgets" />
+      <Button name="Electronics" />
+      <Button name="Travel" />
+      <Button name="GYM" />
+      <Button name="Weather" />
+      <Button name="Cars" />
+      <Button name="Bikes" />
+      <Button name="Railway" />
+      <Button name="Food" />
+      <Button name="OM" />
+      <Button name="Bhakti" />
+      <Button name="Cartoons" />
+      <Button name="Anime" />
+    </div>
+  );
 };
 
 export default ButtonList;
