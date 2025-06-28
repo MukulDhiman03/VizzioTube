a0a06d7095c8e55a5b5c403b55b4ad41c56a3b76
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-06-28 15:27:23 +0530

    Hamburger menu toggler

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  let isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  // early return pattern
+  if (!isMenuOpen) return null;
+
+  return (
+    <div className="col-span-1 w-48 p-5 shadow-lg">
+      <ul>
+        <li>Home</li>
+        <li>Shots</li>
+        <li>Videos</li>
+        <li>Live</li>
+      </ul>
+      <h1 className="font-bold pt-5">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Gamings</li>
+        <li>Sports</li>
+        <li>Movies</li>
+      </ul>
+      <h1 className="font-bold pt-5">Subscriptions</h1>
+      <ul>
+        <li>Music</li>
+        <li>Gamings</li>
+        <li>Sports</li>
+        <li>Movies</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
