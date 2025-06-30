0d96522f034b34f2a81f477d5d1eef5c23e1f930
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-06-30 22:49:18 +0530

    Live chat started

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -30,16 +30,20 @@ const WatchPage = () => {
   return (
     <div className="flex flex-col">
       <div className="px-5">
-        <iframe
-          width="1260"
-          height="600"
-          src={`https://www.youtube.com/embed/${videoId}`}
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe>
+        <div
+        >
+          <iframe
+            width="1260"
+            height="600"
+            src={`https://www.youtube.com/embed/${videoId}`}
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div></div>
       </div>
       <CommentsContainer />
     </div>
